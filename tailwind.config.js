a6eb1065746a98bbf41a9a274cd1e399469e68e4
Elise <elisztomania@gmail.com>
2021-01-11 20:03:42 +0100

    mini front setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'creme': '#F7F4EA',
+        'lila-soft':'#DED9E2',
+        'lila-hard':'#C0B9DD',
+        'new-black':'#161925',
+        'new-gray':'#565554',
+      }
+    },
   },
   variants: {
     extend: {},
