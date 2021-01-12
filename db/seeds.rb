487d6509f88220095e5b854f941a3dd0b4cb431c
Elise <elisztomania@gmail.com>
2021-01-12 12:02:21 +0100

    seed userr

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+
+# Ingredient.destroy_all
+# Quantiy.destroy_all
+# Steps.destroy_all
+# Recipe.destroy_all
+User.destroy_all
+
+puts 'Creating users...'
+  user_admin = User.create(email:"admin@cookiteasy.xyz", password:"getschompignon")
+    user_admin.save!
+  ella = User.create(email:"e@ll.a", password:"ellaellaaa")
+
+puts 'Finished!'
