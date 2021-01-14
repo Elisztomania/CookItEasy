8d3b5c0871fa5b6f91690bcf15c33edf86c2cab7
Elise <elisztomania@gmail.com>
2021-01-14 16:53:17 +0100

    all working

--- app/models/quantity.rb
+++ app/models/quantity.rb
@@ -2,7 +2,7 @@ class Quantity < ApplicationRecord
   belongs_to :ingredient
   belongs_to :recipe
 
-  MESURE = [" ", "g", "Ml", "Cl", "L", "Cc", "Cs", "Tasse", "Pincée(s)", "Tranche(s)", "Conserve(s)", "Pâte(s)"]
+  MESURE = ["g", "Ml", "Cl", "L", "Cc", "Cs", "Tasse(s)", "Pincée(s)", "Tranche(s)", "Conserve(s)", "Pâte(s)", "(rien)"]
 
   validates :q, presence: true
   validates :mesure, inclusion: { in: MESURE }
