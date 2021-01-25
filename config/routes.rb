35cd12d3f4cba57bd836176f87656d1781819b0f
Elise <elisztomania@gmail.com>
2021-01-25 13:13:49 +0100

    route + contro + view created

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'recipes/myrecipes', to: 'recipes#show_my', as: 'my_recipes'
+  get 'recipes/favorites', to: 'recipes#show'
   resources :recipes, only: [:index, :show, :create, :new, :edit, :update] do
     resources :quantities, only: [:create, :new, :edit, :update]
     resources :ingredients, only: [:create, :new]
     resources :steps, only: [:create, :new, :edit, :update]
-    ressource :favorites, only: [:create]
+    resource :favorites, only: [:create]
   end
 
-  ressource :favorites, only: :destroy
+  resources :favorites, only: :destroy
 end
