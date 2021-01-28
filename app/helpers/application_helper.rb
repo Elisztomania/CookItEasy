2c22a53717bb061c02e831df4228a232cdae9557
Elise <elisztomania@gmail.com>
2021-01-28 19:59:53 +0100

    method minutes in hours

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def min_conversion(minutes)
+    if (minutes % 60).zero?
+      hours = minutes / 60
+      return "#{hours}h"
+    elsif minutes > 60
+      hours = minutes / 60
+      rest = minutes % 60
+      return "#{hours}h#{rest}"
+    else
+      "#{minutes} min"
+    end
+  end
 end
